8262c200c0c17942bbe32f4516f9d27c119bc611
Ivan <ivantanweihan@gmail.com>
2020-11-13 15:00:43 +0800

    merge

---
+++ js/dropdown.js
@@ -0,0 +1,17 @@
+// use twitter typeahead
+
+var ingredient_suggestion = new Bloodhound({
+  datumTokenizer: Bloodhound.tokenizers.whitespace,
+  queryTokenizer: Bloodhound.tokenizers.whitespace,
+  // url points to a json file that contains an array of ingredient names, see
+  prefetch: 'JSON/ingredients.json'
+});
+
+// passing in `null` for the `options` arguments will result in the default
+// options being used
+$('#search_box .typeahead').typeahead(null, {
+  highlight: true,
+  name: 'ingredient_suggestion',
+  source: ingredient_suggestion
+});
+

86101ef41a42f96b244488b1a21b61d942a4dcec
Ivan <ivantanweihan@gmail.com>
2020-11-14 19:42:10 +0800

    test

--- login_system/another_page.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once 'protect.php';
-
-$username = $_SESSION['user'];
-
-echo "Hello $username. You are at another page";
-
-echo "<br><a href='logout.php'>Logout</a>";
-?>
--- login_system/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-// clear session data
-$_SESSION = [];
-
-echo "You have been logged out!"
-?>

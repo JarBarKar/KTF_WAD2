6d7c48bbc218ed7d51a2cc3c82515414f2e0a1b7
Aaron <tan.aaron.2019@sis.smu.edu.sg>
2020-11-15 15:10:59 +0800

    added ajax to push to php

--- js/passrecipe_ajax.js
+++ js/passrecipe_ajax.js
@@ -1,10 +1,11 @@
 function passrecipe(){
-    var recipe = document.getElementById("recipeTitle").innerText;
+    var recipe = document.getElementById("recipeID").value;
     const xhr = new XMLHttpRequest();
     xhr.onload = function(){
-        console.log(recipe)
+        console.log(recipe + "Succesful")
+
     }
-    xhr.open('POST','dom.php')
+    xhr.open('POST','dashboard/recipeToSQL.php')
     xhr.setRequestHeader('Content-type','application/x-www-form-urlencoded');
     xhr.send("recipe=" + recipe);
 }
